4b77b32a1406162af537bb4e2fccd759f8914a2a
leada2025 <webdevldd@gmail.com>
2025-08-12 19:07:39 +0530

    veerix

--- front/src/admin/api/Axios.jsx
+++ front/src/admin/api/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://veeerix.onrender.com",
   withCredentials: true, // if you're using cookies for auth
 });
 
